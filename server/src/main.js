f68d0ac36a3ce49701382067381ff9d3301740e6
Szwagier <krystianek@gmail.com>
2025-08-22 21:17:04 +0200

    Completed joining to rooms, set it up to 2 sockets max

--- server/src/main.js
+++ server/src/main.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth-route");
 const { Server } = require("socket.io");
 const http = require("http");
+const room_join = require("./scripts/rooms.js");
 
 const app = express();
 require("./database");
@@ -25,44 +26,52 @@ app.use("/api", authRouter);
 const httpserver = http.createServer(app);
 
 const io = new Server(httpserver, {
-    cors: {
-        origin: "http://localhost:5173",
-        credentials: true,
-    },
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
 });
 
 let RoomsIndex = 0;
 
 let fullChat = [];
 
+
+// Connection
 io.on("connection", (socket) => {
-    RoomsIndex++;
-    socket.join(`room${RoomsIndex}`);
-
-
-    if(RoomsIndex > 1) socket.join([`room${RoomsIndex}`, `room${RoomsIndex - 1}`]);
-
+  
+  let roomToJoin = room_join(io.sockets.adapter.rooms, RoomsIndex);
+  
+  if(roomToJoin) socket.join(`${roomToJoin}`);
+  else socket.join(`room${++RoomsIndex}`);
+  
+  // Console logs
+  console.log(io.sockets.adapter.rooms, "all rooms");
+  console.log(io.sockets.adapter.rooms.size, "amount of rooms");
+  console.log(io.sockets.adapter.rooms.get("room1")?.size, "size of room1");
+  
+  // Emits
+  socket.emit("user-joined", "hello");
+  
+  console.log(io.sockets.adapter.sids.size, "Number of sockets");
+  
+  // Ons
+  socket.on("send-message", (arrayOfMessages) => {
+    fullChat.push(arrayOfMessages);
+    console.log(arrayOfMessages, "messages");
+    io.to("room1").emit("send-message", fullChat);
+  });
+  
+  // Disconnected
+  socket.on("disconnect", () => {
+    io.to("room1").emit("receive-chat", fullChat);
+    console.log("disconnected");
     console.log(io.sockets.adapter.rooms, "all rooms");
     console.log(io.sockets.adapter.rooms.size, "amount of rooms");
-    console.log(io.sockets.adapter.rooms.get("room1").size, "size of room1");
-
-    socket.emit("user-joined", "hello");
-
-    console.log(io.sockets.adapter.sids.size, "Number of sockets");
-
-    socket.on("send-message", (arrayOfMessages) => {
-      fullChat.push(arrayOfMessages);
-      console.log(arrayOfMessages, "messages");
-      io.to("room1").emit("send-message", fullChat);
-    })
-
-    socket.on("disconnect", () => {
-      RoomsIndex--;
-      io.to("room1").emit("receive-chat", fullChat);
-      console.log("disconnected");
-    })
-})
-
+    console.log(io.sockets.adapter.rooms.get("room1")?.size, "size of room1");
+  });
+  console.log(socket.rooms);
+});
 
 const PORT = process.env.PORT || 5000;
 
---
+++ server/src/scripts/rooms.js
@@ -0,0 +1,40 @@
+function room_join(rooms, roomsIndex){
+
+    const collator = new Intl.Collator(undefined, { numeric: true });
+
+    const ROOMS_NAMES = [];
+
+    for(const keys of rooms.keys()){
+        console.log(keys);
+        ROOMS_NAMES.push(keys);
+    }
+
+    const filtered = ROOMS_NAMES.filter((x) => x.length < 11);
+
+    console.log(filtered, "filtered");
+
+    if(filtered.length > 0){
+
+        const result = filtered.map(x => {
+            if(rooms.get(x).size < 2) return x;
+        })
+        if(result.length > 1){
+            result.sort(collator.compare);
+            result.splice(1);
+            return `${result}`;
+        }
+        console.log(result, "answer");
+        return `${result}`;
+    } 
+
+    return;
+}
+
+// const roomsSubsitute = new Map([
+//     ["AJSDHKASHDJKSHDKASHDa", 1],
+//     ["asda", 2]
+// ]);
+
+// room_join(roomsSubsitute);
+
+module.exports = room_join;
