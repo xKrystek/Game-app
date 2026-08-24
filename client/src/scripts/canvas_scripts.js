4590f5f7fc74e1e8e6c1eb6f1d25534d21f95fab
Krystian <xKrystek@fromWerka.com>
2026-08-24 13:37:08 +0200

    board, ship, game_engine classes created, ship dragging completed

--- client/src/scripts/canvas_scripts.js
+++ client/src/scripts/canvas_scripts.js
@@ -1,53 +1,105 @@
-/**
- * @param {CanvasRenderingContext2D} ctx
- */
-export function drawBoard(
-  ctx,
-  numberOfFields,
-  border_size,
-  left,
-  top,
-  right,
-  bottom,
-  gap = 0
-) {
-  const Field_size = (border_size - gap * (numberOfFields - 1)) / numberOfFields;
+export class Game_Engine {
+  /**
+   * @type {CanvasRenderingContext2D}
+   */ ctx;
+  constructor(ctx) {
+    this.ctx = ctx;
+  }
 
-  console.log(Field_size, "size of the one box");
-  console.log(border_size, "width of the whole board");
-    if (gap) {
-      for (let i = 0; i < numberOfFields; i++) {
-        for(let j = 0; j < numberOfFields; j++){
-          ctx.strokeRect(left + j * (Field_size + gap), top + i * (Field_size + gap), Field_size, Field_size);
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  }
+}
+
+export class Board {
+  /**
+   * @type {CanvasRenderingContext2D}
+   */ ctx;
+  constructor(
+    ctx,
+    numberOfFields,
+    border_size,
+    left,
+    top,
+    right,
+    bottom,
+    gap = 0,
+  ) {
+    this.ctx = ctx;
+    this.numberOfFields = numberOfFields;
+    this.border_size = border_size;
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.gap = gap;
+  }
+  drawBoard() {
+    const Field_size =
+      (this.border_size - this.gap * (this.numberOfFields - 1)) /
+      this.numberOfFields;
+
+    if (this.gap) {
+      for (let i = 0; i < this.numberOfFields; i++) {
+        for (let j = 0; j < this.numberOfFields; j++) {
+          this.ctx.strokeRect(
+            this.left + j * (Field_size + this.gap),
+            this.top + i * (Field_size + this.gap),
+            Field_size,
+            Field_size,
+          );
         }
       }
     } else {
-      for (let i = 0; i <= numberOfFields; i++) {
+      for (let i = 0; i <= this.numberOfFields; i++) {
         // horizontal
-        ctx.beginPath();
-        ctx.moveTo(left, top + i * Field_size);
-        ctx.lineTo(right, top + i * Field_size);
-        ctx.stroke();
-        ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.left, this.top + i * Field_size);
+        this.ctx.lineTo(this.right, this.top + i * Field_size);
+        this.ctx.stroke();
+        this.ctx.closePath();
 
         // vertical
-        ctx.beginPath();
-        ctx.moveTo(left + i * Field_size, top);
-        ctx.lineTo(left + i * Field_size, bottom);
-        ctx.stroke();
-        ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.left + i * Field_size, this.top);
+        this.ctx.lineTo(this.left + i * Field_size, this.bottom);
+        this.ctx.stroke();
+        this.ctx.closePath();
       }
     }
+  }
 }
 
-/**
- * @param {CanvasRenderingContext2D} ctx
- */
+export class Ship {
+  /**
+   * @type {CanvasRenderingContext2D}
+   */
+  ctx;
 
-export function drawShip(ctx, size, x, y, shipSizeUnit){
-  const SHIP_HEIGHT = shipSizeUnit * size;
-  const SHIP_WIDTH = shipSizeUnit;
+  constructor(ctx, size, x, y, shipSizeUnit) {
+    this.ctx = ctx;
+    this.size = size;
+    this.shipSizeUnit = shipSizeUnit;
+    this.SHIP_HEIGHT = shipSizeUnit * size;
+    this.SHIP_WIDTH = shipSizeUnit;
+    this.x = x - this.SHIP_WIDTH - 15;
+    this.y = y;
+  }
 
-  ctx.strokeRect(x - SHIP_WIDTH - 15, y, SHIP_WIDTH, SHIP_HEIGHT);
-  ctx.strokeText("6", x - SHIP_WIDTH / 3 * 2 - 15, y + 15);
+  drawShip(x = this.x, y = this.y) {
+    this.ctx.strokeRect(x, y, this.SHIP_WIDTH, this.SHIP_HEIGHT);
+    this.ctx.font = "15px Arial";
+    const metricsOfText = this.ctx.measureText("6");
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "middle";
+    const actualFontHeight =
+      metricsOfText.actualBoundingBoxAscent +
+      metricsOfText.actualBoundingBoxDescent;
+    this.ctx.strokeText("6", x + this.SHIP_WIDTH / 2, y + actualFontHeight);
+    this.setPosition(x, y);
+  }
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
--- client/src/ships/Canvas.jsx
+++ client/src/ships/Canvas.jsx
@@ -1,16 +1,48 @@
 import { useLayoutEffect, useRef, useState } from "react";
 
-import { drawBoard, drawShip } from "../scripts/canvas_scripts";
+import { Board, Game_Engine, Ship } from "../scripts/canvas_scripts.js";
 
 function Canvas() {
   const canvasRef = useRef(null);
   const [WINDOW_WIDTH, setWINDOW_WIDTH] = useState(window.innerWidth);
   const [WINDOW_HEIGHT, setWindow_HEIGHT] = useState(window.innerHeight);
+  const shipRef = useRef(null);
+  const draggingShip = useRef(false);
+  const offset = useRef(null);
 
-  window.addEventListener("resize", () => {
+  const handleResize = () => {
     setWINDOW_WIDTH(window.innerWidth);
     setWindow_HEIGHT(window.innerHeight);
-  });
+  };
+
+  const setOffset = (e, ship) => {
+    if (
+      e.clientX > ship.x &&
+      e.clientY > ship.y &&
+      e.clientX < ship.x + ship.SHIP_WIDTH &&
+      e.clientY < ship.y + ship.SHIP_HEIGHT
+    ) {
+      offset.current = {
+        x: ship.x - e.clientX,
+        y: ship.y - e.clientY
+      };
+      draggingShip.current = true;
+    }
+  };
+
+  /**
+    @param {Ship} ship 
+    @param {Game_Engine} gameEngine
+    @param {Board} board
+   */
+
+  const dragShips = (e, ship, gameEngine, board) => {
+    if (draggingShip.current) {
+      gameEngine.clearCanvas();
+      board.drawBoard();
+      ship.drawShip(e.clientX + offset.current.x, e.clientY + offset.current.y);
+    }
+  };
 
   useLayoutEffect(() => {
     /** @type {HTMLCanvasElement} */
@@ -25,16 +57,35 @@ function Canvas() {
 
     const BORDER_SIZE = HEIGHT < WIDTH ? WIDTH : HEIGHT;
     const NUMBER_OF_FIELDS = 10;
-    const SHIP_SIZE_UNIT = BORDER_SIZE / NUMBER_OF_FIELDS;
     const LEFT = canvas.width / 2 - BORDER_SIZE / 2;
     const TOP = canvas.height / 2 - BORDER_SIZE / 2;
     const RIGHT = canvas.width / 2 + BORDER_SIZE / 2;
     const BOTTOM = canvas.height / 2 + BORDER_SIZE / 2;
 
+    const SHIP_SIZE_UNIT = BORDER_SIZE / NUMBER_OF_FIELDS;
+
+    const GAME_ENGINE = new Game_Engine(ctx);
+
+    const SHIP_6 = new Ship(ctx, 6, WINDOW_WIDTH, TOP, SHIP_SIZE_UNIT);
+    shipRef.current = SHIP_6;
+
     ctx.strokeStyle = "white";
-    // ctx.strokeRect(LEFT, TOP, WIDTH, HEIGHT);
-    drawBoard(ctx, NUMBER_OF_FIELDS, BORDER_SIZE, LEFT, TOP, RIGHT, BOTTOM, 4);
-    drawShip(ctx, 6, WINDOW_WIDTH, TOP, SHIP_SIZE_UNIT);
+    const ShipsBoard = new Board(ctx, NUMBER_OF_FIELDS, BORDER_SIZE, LEFT, TOP, RIGHT, BOTTOM, 4);
+    ShipsBoard.drawBoard();
+
+    SHIP_6.drawShip();
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("mousedown", (e) => setOffset(e, SHIP_6));
+    window.addEventListener("mousemove", (e) => dragShips(e, SHIP_6, GAME_ENGINE, ShipsBoard));
+    window.addEventListener("mouseup", () => {
+      draggingShip.current = false;
+    });
+
+    return () => {
+      // window.removeEventListener("mousedown");
+      window.removeEventListener("resize", handleResize);
+    };
   }, [WINDOW_WIDTH, WINDOW_HEIGHT]);
 
   return <canvas width={100} height={100} ref={canvasRef}></canvas>;
